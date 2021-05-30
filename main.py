ec919b94198207bf648f7501f7293eedd2702f29
Adrian Haasler García <dev@adrianhaasler.com>
2021-05-30 17:25:56 +0200

    feat(attach): filter sessions based on user input
    Refs: closes #2

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         items = []
+        search = event.get_argument() or ""
         pipe = os.popen("if tmux ls > /dev/null 2>&1; then tmux ls; else echo none; fi")
         terminal_binary = extension.preferences['terminal_binary']
         console_parameters_attach = extension.preferences['console_parameters_attach']
@@ -24,14 +25,15 @@ class KeywordQueryEventListener(EventListener):
         output = pipe.read()
         if output.splitlines()[0] != "none":
             for line in output.splitlines():
-                items.append(
-                    ExtensionResultItem(
-                        icon='images/tmux.png',
-                        name='Attach session %s' % line.split(' ')[0],
-                        description=line,
-                        on_enter=RunScriptAction(terminal_binary + ' ' + console_parameters_attach % line.split(' ')[0], None)
+                if not search or search.lower() in line.split(' ')[0].lower():
+                    items.append(
+                        ExtensionResultItem(
+                            icon='images/tmux.png',
+                            name='Attach session %s' % line.split(' ')[0],
+                            description=line,
+                            on_enter=RunScriptAction(terminal_binary + ' ' + console_parameters_attach % line.split(' ')[0], None)
+                        )
                     )
-                )
         else:
             items.append(
                 ExtensionResultItem(
